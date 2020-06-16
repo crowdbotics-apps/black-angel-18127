be4e63ba80c4749b23f89bac403db7e4b3c1ff72
Crowdbotics <team@crowdbotics.com>
2020-06-16 00:10:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/black_angel_18127/MainActivity.java
@@ -0,0 +1,15 @@
+package com.black_angel_18127;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "black_angel_18127";
+    }
+}
